245af814d95b03ddf2043c8836093544dbcd0c53
samanthashleyrose <samantha.rose327@gmail.com>
2024-03-02 16:19:59 -0500

    Began set up for login and sign up pages

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,13 @@
 export default function Home() {
     return (
         <div>
-            <h2>This is the home page</h2>
+            <Title />
+            <h2>Login</h2>
+            <div class="btn-container">
+                <button id="login">login</button>
+                <br/>
+                    <button id="sign-up">sign up</button>
+            </div>
         </div>
     );
 }
---
+++ client/src/pages/SignUp.jsx
@@ -0,0 +1,22 @@
+import Title from "../components/Title"
+
+export default function SignUp() {
+    return (
+        <div>
+            <Title />
+            <div class="login-signup-container">
+                <h3 id="h3-title">sign up</h3>
+                <form id="sign-up-form">
+                    <input id="username-input" type="text" placeholder="username" name="username" autocomplete="off" />
+                    <br />
+                    <input id="email-input" type="text" placeholder="email" name="email" autocomplete="off" />
+                    <br />
+                    <input id="pass-input" type="password" placeholder="password" name="password" autocomplete="off" />
+                </form>
+                <div class="btn-container">
+                    <button id="sign-up">sign up</button>
+                </div>
+            </div>
+        </div>
+    );
+}
