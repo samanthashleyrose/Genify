44105cf58c9ddeee08c93dc50056ac8565457d99
samanthashleyrose <samantha.rose327@gmail.com>
2024-03-02 17:01:34 -0500

    Created Playlist Model

---
+++ server/models/Playlist.js
@@ -0,0 +1,42 @@
+const { Schema, model } = require('mongoose');
+
+const playlistSchema = new Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        description: {
+            type: String,
+            default: '',
+        },
+        tracks: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Track',
+            },
+        ],
+        owner: {
+            type: Schema.Types.ObjectId,
+            ref: 'User',
+            required: true,
+        },
+        created_at: {
+            type: Date,
+            default: Date.now,
+        },
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+    }
+);
+
+playlistSchema.virtual('songCount').get(function () {
+    return this.tracks.length;
+});
+
+const Playlist = model('Playlist', playlistSchema);
+
+module.exports = Playlist;
