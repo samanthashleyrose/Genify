286e029553c4fcf93a241717f063804586f5b911
samanthashleyrose <samantha.rose327@gmail.com>
2024-03-02 11:38:47 -0500

    Worked on set up for client folder. Main.jsx, App.jsx, index.html, Home.jsx, CallbackPage.jsx

--- client/App.jsx
+++ client/App.jsx
@@ -1,11 +1,37 @@
 import { Outlet } from 'react-router-dom';
+import React from 'react'
 
-function App() {
+import { SpotifyApiContext } from 'react-spotify-api'
+import Cookies from 'js-cookie'
+
+import { SpotifyAuth, Scopes } from 'react-spotify-auth'
+import 'react-spotify-auth/dist/index.css'
+require('dotenv').config();
+
+const App = () => {
+  const [token, setToken] = React.useState(Cookies.get("spotifyAuthToken"))
+  
   return (
     <main>
       <Outlet />
+      <div>
+        {token ? (
+          <SpotifyApiContext.Provider value={token}>
+            {/* Your Spotify Code here */}
+            <p>You are authorized with token: {token}</p>
+          </SpotifyApiContext.Provider>
+        ) : (
+          // Display the login page
+          <SpotifyAuth
+            redirectUri='http://localhost:3027/callback'
+            clientID={process.env.SPOTIFY_CLIENT_ID}
+            scopes={[Scopes.userReadPrivate, 'user-read-email', 'playlist-modify-public']} // either style will work
+            onAccessToken={(token) => setToken(token)}
+          />
+        )}
+      </div>
     </main>
-  );
+  )
 }
 
 export default App;
