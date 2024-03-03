47c342fd33af462014556978e128569a16a1f323
samanthashleyrose <samantha.rose327@gmail.com>
2024-03-03 16:44:38 -0500

    Testing code adjustments to access users spotify accounts.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,46 @@
-import { Outlet } from 'react-router-dom';
-import Footer from './components/Footer';
-import React from 'react'
+// import { Outlet } from 'react-router-dom';
+// import Footer from './components/Footer';
+// import React from 'react'
+
+// const App = () => {
+//   return (
+//     <main>
+//       <Outlet />
+//       <Footer />
+//     </main>
+//   )
+// }
+
+// export default App;
+
+import { Outlet } from "react-router-dom";
+import Cookies from 'js-cookie'
+import { SpotifyApiContext } from "react-spotify-api";
+import Footer from "./components/Footer";
+import React, { createContext, useContext } from "react";
+
+const tokenContext = createContext();
+
+export const useTokenContext = () => useContext(tokenContext);
+
+const TokenProvider = (props) => {
+  const [token, setToken] = React.useState(Cookies.get("spotifyAuthToken"));
+  return (
+    <SpotifyApiContext.Provider value={token}>
+      <tokenContext.Provider value={{ token, setToken }} {...props} />
+    </SpotifyApiContext.Provider>
+  );
+};
 
 const App = () => {
   return (
-    <main>
-      <Outlet />
-      <Footer />
-    </main>
-  )
-}
+    <TokenProvider>
+      <main>
+        <Outlet />
+        <Footer />
+      </main>
+    </TokenProvider>
+  );
+};
 
 export default App;
