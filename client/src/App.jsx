c23423130621b94e2d7683bbf70a8df8dd150ff8
samanthashleyrose <samantha.rose327@gmail.com>
2024-03-03 12:31:27 -0500

    Moved to LinkSpotify.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,15 +2,7 @@ import { Outlet } from 'react-router-dom';
 import Footer from './components/Footer';
 import React from 'react'
 
-import { SpotifyApiContext } from 'react-spotify-api'
-import Cookies from 'js-cookie'
-
-import { SpotifyAuth, Scopes } from 'react-spotify-auth'
-import 'react-spotify-auth/dist/index.css'
-
 const App = () => {
-  const [token, setToken] = React.useState(Cookies.get("spotifyAuthToken"))
-  
   return (
     <main>
       <Outlet />
