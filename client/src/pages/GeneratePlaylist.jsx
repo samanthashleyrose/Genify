c0b75b48436f7f81a8038215903c455cc95bd642
samanthashleyrose <samantha.rose327@gmail.com>
2024-03-04 21:31:09 -0500

    Generated playlist css

--- client/src/pages/GeneratePlaylist.jsx
+++ client/src/pages/GeneratePlaylist.jsx
@@ -11,12 +11,16 @@ export default function GeneratePlaylist() {
             <HeaderwithNav />
             <div className='generate-playlist-page'>
                 <h3 className='h3-title'>Generated Playlist</h3>
+                <div className="playlists-container">
+                    <div className="generated-playlist">
+                        <img src="src/assets/imgs/spotify-logo.png" alt="Spotify Logo" className='playlist-img' />
+                        <h4 className='playlist-name'>{playlistData.playlistName}</h4>
+                    </div>
+                </div>
+                <h3 className='h3-title'>About this Playlist</h3>
                 <p className='p-title'>genres: {playlistData.genres.join(', ')}</p>
                 <p className='p-title'>artists: {playlistData.artists.map(artist => artist.name).join(', ')}</p>
-                <p className='p-title'>playlist name: {playlistData.playlistName}</p>
-                <p className='p-title'>song count: {playlistData.songCount}</p>
-            </div>
-            <div className="gen-btn-container">
+                <p className='p-title'>{playlistData.songCount} songs</p>
                 <Link to="/"><button id='link-to-spotify-btn'>Add to your Spotify Account</button></Link>
             </div>
         </div>
