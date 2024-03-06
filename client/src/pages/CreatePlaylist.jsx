5e93bfa81c2e56f293ccfc5f9b2f4e34a4c8c514
samanthashleyrose <samantha.rose327@gmail.com>
2024-03-05 20:37:14 -0500

    Added value disclaimer

--- client/src/pages/CreatePlaylist.jsx
+++ client/src/pages/CreatePlaylist.jsx
@@ -10,7 +10,7 @@ export default function CreatePlaylist() {
             <TitlewithNav />
             <div className='create-playlist-page'>
                 <h3 className='h3-title'>generate a new playlist</h3>
-                <h4 className='instructions'>To generate a new playlist, we have to gather some information about your interests. Let's get started!</h4>
+                <h4 className='instructions'>To generate a new playlist, we have to gather some information about your interests. You may select up to 5 values total. Let's get started!</h4>
             </div>
             <div className="btn-container">
                 <Link to="/SelectGenres" className={currentPage === '/SelectGenres'}><button id='gen-playlist'>create a playlist</button></Link>
