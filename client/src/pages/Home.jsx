1f9a6f1897c5f863baed6b7add9f7f1f4181d772
samanthashleyrose <samantha.rose327@gmail.com>
2024-03-09 11:19:04 -0500

    Working towards alert message functionality

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,19 +1,31 @@
-import TitlewithNav from '../components/TitlewithNav';
+import React, { useState } from 'react';
 import LinkSpotify from '../components/LinkSpotify';
 import { Link } from 'react-router-dom';
+import Message from '../components/Message';
 
 import Auth from '../utils/auth';
 
 export default function Home() {
+    const [showAlert, setShowAlert] = useState(false);
 
     // Handles logging out a user
     const logout = (event) => {
         event.preventDefault();
         Auth.logout();
     }
+    // Message displays informing user they must link their spotify account before they can generate a playlist
+    const linkSpotifyAlert = (event) => {
+        event.preventDefault();
+        setShowAlert(true);
+    }
+
+    // Function to close the alert message
+    const closeAlert = () => {
+        setShowAlert(false); // Hide the alert message
+    }
 
     return (
-        <div>
+        <div className='home-container'>
             <div className="header-nav-container">
                 <div className="titles-container">
                     <h1 id="sm-title">Genify</h1>
@@ -26,9 +38,10 @@ export default function Home() {
             <div className="btn-container">
                 <Link to="/ViewPlaylists"><button id='view-playlists'>view playlists</button></Link>
                 <br />
-                <Link to="/CreatePlaylist"><button id='gen-playlist'>generate playlist</button></Link>
+                <Link to="/CreatePlaylist"><button id='gen-playlist' onClick={linkSpotifyAlert}>generate playlist</button></Link>
             </div>
             <LinkSpotify />
+            {showAlert && <Message onClose={closeAlert}/>}
         </div>
     );
 }
