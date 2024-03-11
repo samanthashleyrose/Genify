7919f968d2bc136c0c8893f8f436431e8167dc85
samanthashleyrose <samantha.rose327@gmail.com>
2024-03-10 20:21:08 -0400

    Logout user after 2 hours

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -2,37 +2,42 @@ import decode from 'jwt-decode';
 
 class AuthService {
 
-    loggedIn() {
-      const token = this.getToken();
-      return token && !this.isTokenExpired(token) ? true : false;
-    }
-  
-    isTokenExpired(token) {
-      try {
-        const decoded = decode(token);
-        if (decoded.exp < Date.now() / 1000) {
-          localStorage.removeItem('id_token');
-          return true;
-        } else 
-        return false;
-      } catch (err) {
+  loggedIn() {
+    const token = this.getToken();
+    return token && !this.isTokenExpired(token) ? true : false;
+  }
+
+  isTokenExpired(token) {
+    try {
+      const decoded = decode(token);
+      // Check if token expiration time is within the next 2 hours
+      const expirationTime = decoded.exp * 1000;
+      const twoHoursFromNow = Date.now() + 2 * 60 * 60 * 1000;
+      if (expirationTime < twoHoursFromNow) {
+        localStorage.removeItem('id_token');
+        window.location.assign('/Login');
+        return true;
+      } else {
         return false;
       }
-    }
-  
-    getToken() {
-      return localStorage.getItem('id_token');
-    }
-  
-    login(idToken) {
-      localStorage.setItem('id_token', idToken);
-      window.location.assign('/Home');
-    }
-  
-    logout() {
-      localStorage.removeItem('id_token');
-      window.location.assign('/Login');
+    } catch (err) {
+      return false;
     }
   }
-  
-  export default new AuthService();
+
+  getToken() {
+    return localStorage.getItem('id_token');
+  }
+
+  login(idToken) {
+    localStorage.setItem('id_token', idToken);
+    window.location.assign('/Home');
+  }
+
+  logout() {
+    localStorage.removeItem('id_token');
+    window.location.assign('/Login');
+  }
+}
+
+export default new AuthService();
